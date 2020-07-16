8befb69ee6ccc2fe1a5b0914740b522d2cc9104d
sayali1098 <sauamritkar@gmail.com>
2020-07-16 16:38:50 +0530

    FSD0620B102

--- dml-joins.sql
+++ dml-joins.sql
@@ -1,1 +1,191 @@
+SELECT * FROM role INNER JOIN user_new
+ON role.id=user_new.id
+ORDER BY user_new.name ASC, role.name ASC;
 
+SELECT * FROM role
+RIGHT JOIN user_new
+ON role.id=user_new.id
+ORDER BY user_new.name ASC, role.name ASC;
+
+select user_new.name from user_new INNER JOIN role
+ON user_new.role_id = role.id;
+
+SELECT user_new.phonenumber, user_new.emailid, profile.batch, department.name, profile.designation, experience.company_name 
+from user_new
+INNER JOIN profile ON user_new.role_id = profile.id
+INNER JOIN degree  ON profile.degree_id = degree.id
+INNER JOIN department ON degree.department_id = department.id
+INNER JOIN experience  ON profile.id = experience.profile_id
+WHERE user_new.name = 'PROGRAD' AND experience.currentvalue=3; 
+
+SELECT user_new.name, skill.name from user_new 
+INNER JOIN role ON user_new.role_id=role.id
+INNER JOIN profile ON  user_new.profile_id = profile.id
+INNER JOIN profile_skills ON profile.id = profile_skills.skill_id
+where role.name = 'ALUMINI'
+ORDER BY user_new.name, skill.name;
+
+select user_new.name, experience.company_name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join experience on user_new.id = experience.id
+where role.name = 'Alumini'
+order by user_new.name,experience.company_name;
+
+select user_new.name, experience.company_name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join experience on user_new.id = experience.id
+where role.name = 'Alumini'
+order by user_new.name,experience.company_name;
+
+select user_new.name, experience.company_name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join experience on user_new.id = experience.id
+where role.name = 'Alumini' and experience.currentValue=1
+order by user_new.name;
+
+select user_new.name, experience.company_name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join experience on user_new.id = experience.id
+where role.name = 'Alumini' and experience.currentValue=1 and profile.batch='2008'
+order by user_new.name;
+
+select user_new.name, user_new.emailid, user_new.phonenumber, profile.address from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+where role.name='Alumni' and user_new.profile_id is not null
+order by user_new.name;
+
+select user_new.name, user_new.emailid, user_new.phonenumber, profile.address from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+where role.name='Alumni' and profile.batch='2008'
+order by user_new.name;
+
+select user_new.name, user_new.emailid, user_new.phonenumber, profile.address from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join degree on user_new.id = degree.id
+where role.name='Alumni' and degree.name='BSC_CT'
+order by user_new.name;
+
+select user_new.name, user_new.emailid, user_new.phonenumber, profile.address from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join degree on user_new.id = degree.id
+inner join department on user_new.id = department.id
+where role.name='Alumni' and department.name='CSE'
+order by user_new.name;
+                     
+select user_new.name, higher_degree.university_name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join higher_degree on user_new.id = higher_degree.id
+inner join department on user_new.id = department.id
+where role.name='Alumni' and higher_degree.degree_name is not null
+order by user_new.name,higher_degree.university_name;
+
+select user_new.name, higher_degree.university_name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join higher_degree on user_new.id = higher_degree.id
+inner join department on user_new.id = department.id
+where role.name='Alumni' and higher_degree.degree_name is not null
+order by user_new.name,higher_degree.university_name;
+
+select user_new.name, user_new.emailid, user_new.phonenumber, profile.address from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join degree on user_new.id = degree.id
+inner join higher_degree on user_new.id = higher_degree.id
+inner join department on user_new.id = department.id
+where role.name='Alumni' and higher_degree.university_name='Texas University'
+order by user_new.name;
+
+select user_new.name, profile.batch from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+where role.name='Alumni' and profile.gender='female'
+order by user_new.name;
+
+select user_new.name, profile.batch,degree.name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join degree on user_new.id = degree.id
+where role.name='Alumni' and profile.gender='female'
+order by user_new.name;
+
+select user_new.name, profile.batch,degree.name,department.name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join degree on user_new.id = degree.id
+inner join department on user_new.id= department.id
+where role.name='Alumni' and profile.gender='female'
+order by user_new.name;
+
+select degree.name, department.name from degree
+inner join department on degree.department_id= department.id
+order by degree.name;
+
+select user_new.name, profile.designation from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+where role.name='Alumni' and profile.gender='male'
+order by user_new.name;
+
+select user_new.name, profile.designation from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join experience on user_new.id = experience.id
+where role.name='Alumni' and experience.currentValue=1 and experience.company_name='TCS'
+order by user_new.name;
+
+select user_new.name, querytable.content from user_new
+inner join querytable on user_new.role_id = querytable.id
+order by user_new.name;
+
+select user_new.name, querytable.content from user_new
+inner join querytable on user_new.role_id = querytable.id
+where querytable.parent_id is not null
+order by user_new.name;
+
+select user_new.name, post.content from user_new
+inner join post on user_new.role_id = post.id
+order by user_new.name;
+
+select user_new.name, post.content from user_new
+inner join post on user_new.role_id = post.id
+inner join post_type on post.type_id = post_type.id
+where  post_type.name='Technology'
+order by user_new.name,post.dateof;
+
+select user_new.name, post.content from user_new
+inner join post on user_new.role_id = post.id
+inner join post_type on post.type_id = post_type.id
+where post.dateof='2013'
+order by user_new.name,post.dateof,post.content;
+
+
+select user_new.name, department.name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join degree on user_new.id = degree.id
+inner join department on user_new.id= department.id
+inner join profile_skills on user_new.profile_id = profile_skills.profile_id
+inner join skills on profile_skills.skill_id = skills.id
+where skills.name='Programming'
+order by user_new.name;
+
+select user_new.name, events.name from user_new
+inner join events on user_new.id = events.id
+where role.name='Alumni'
+order by user_new.name,events.dateof,events.name desc;
+
+select user_new.name, events.name from user_new
+inner join events on user_new.id = events.id
+inner join role on user_new.role_id = role.id
+where role.name='Alumni'
+order by user_new.name,events.dateof;
--- insert-queries.sql
+++ insert-queries.sql
@@ -1,30 +1,57 @@
 insert into role(id,name)values(1,'designer');
 insert into role(id,name)values(2,'guide');
-
+insert into role(id,name)values(3,'cricketer');
+insert into role(id,name)values(4,'model');
+insert into role(id,name)values(5,'artist');
+insert into role(id,name)values(6,'dancer');
 select * from role;
 
 insert into skill(id,description,name)values(1,'self learning','learn');
 insert into skill(id,description,name)values(2,'technical skills','coding');
+insert into skill(id,description,name)values(3,'management skills','leadership');
+insert into skill(id,description,name)values(4,'observation skills','listening');
+insert into skill(id,description,name)values(5,'artist','acting');
+insert into skill(id,description,name)values(6,'social help','charity');
 select * from skill;
 
 insert into post_type(id,name)values(1,'business');
 insert into post_type(id,name)values(2,'business');
+insert into post_type(id,name)values(3,'service');
+insert into post_type(id,name)values(4,'entrepreneur');
+insert into post_type(id,name)values(5,'self-employed');
+insert into post_type(id,name)values(6,'labour');
 select * from post_type;
 
 
 insert into department(id,name)values(1,'bpo');
 insert into department(id,name)values(2,'IT');
+insert into department(id,name)values(3,'HR');
+insert into department(id,name)values(4,'NETWORK');
+insert into department(id,name)values(5,'CORE');
+insert into department(id,name)values(6,'INSUARANCE');
 select * from department;
 
 insert into degree(id,department_id,name)values(1,1,'EEE');
 insert into degree(id,department_id,name)values(2,2,'ECE');
+insert into degree(id,department_id,name)values(3,3,'CS');
+insert into degree(id,department_id,name)values(4,4,'CIVIL');
+insert into degree(id,department_id,name)values(5,5,'MECH');
+insert into degree(id,department_id,name)values(6,6,'IT');
+select * from degree;
 
 insert into profile(id,address,batch,degree_id,designation,gender)values(1,'London','En',1,'HR','Male');
 insert into profile(id,address,batch,degree_id,designation,gender)values(2,'Chennai','For',2,'Manager','Female');
+insert into profile(id,address,batch,degree_id,designation,gender)values(3,'Pune','En',3,'MD','Male');
+insert into profile(id,address,batch,degree_id,designation,gender)values(4,'Banglore','For',4,'CEO','Female');
+insert into profile(id,address,batch,degree_id,designation,gender)values(5,'Mumbai','En',5,'Sales','Male');
+select * from profile;
 
 insert into higher_degree(id,degree_name,university_name,profile_id)values(1,'BE','MIT',1);
 insert into higher_degree(id,degree_name,university_name,profile_id)values(2,'BE','STANFORD',2);
-
+insert into higher_degree(id,degree_name,university_name,profile_id)values(3,'ME','DELLAS',3);
+insert into higher_degree(id,degree_name,university_name,profile_id)values(4,'MS','TEXAS',4);
+insert into higher_degree(id,degree_name,university_name,profile_id)values(5,'BE','SPPU',5);
+select * from higher_degree;
 
 insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(2,'CTS',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),2);
 
@@ -35,6 +62,7 @@ insert into project(id,name,number_of_members,profile_id,short_description)value
 
 insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(1,'abcd@efgh.com','abcd','efgh',9884642005,1,1,'abcdefgh');
 insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(2,'ijkl@mnop.com','ijkl','mnop',9845621745,2,2,'ijklmnop');
+insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(3,'vriv@mnop.com','mnbop','kiji',487845654,3,3,'lmvnfsop');
 
 insert into querytable(id,content,dateof,parent_id,user_id)values(1,'Alumini management',TO_DATE('2008/11/11','YYYY/MM/DD'),1,1);
 insert into querytable(id,content,dateof,parent_id,user_id)values(2,'Library management',To_DATE('2008/11/19','YYYY/MM/DD'),2,2);
